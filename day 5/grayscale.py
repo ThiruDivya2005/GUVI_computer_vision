3791e4b12e8ca0859525792396176d833be2e62b
ThiruDivya2005 <144613704+ThiruDivya2005@users.noreply.github.com>
2025-08-22 11:46:27 +0530

    Add files via upload

---
+++ day 5/grayscale.py
@@ -0,0 +1,8 @@
+
+import cv2
+import matplotlib.pyplot as plt
+
+img = cv2.imread("cheta.jpeg")
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+plt.imshow(cv2.cvtColor(gray,cv2.COLOR_BGR2RGB))
+plt.show()
